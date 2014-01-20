c1d3fda0b15eef0dd45c3b6c5c9d93054701331b
a478421 <a478421@fmr.com>
2014-01-20 18:44:41 +0000

    added web initilaizer

--- todo/todo-service/src/main/java/com/mtt/service/ServiceAppContext.java
+++ todo/todo-service/src/main/java/com/mtt/service/ServiceAppContext.java
@@ -3,9 +3,7 @@ package com.mtt.service;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
 import com.mtt.presistance.PresistanceAppContext;
-import com.mtt.presistance.repository.ItemRepository;
 
 
 @Configuration
